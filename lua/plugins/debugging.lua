5c7efa2883f6cad3a0291f0dca0167c6f83a463b
Chih Ying <spacetime0311@gmail.com>
2024-01-17 10:57:16 +0800

    feat: DAP starter

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,28 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		require("dapui").setup()
+
+		-- open and close the windows automatically
+		local dap, dapui = require("dap"), require("dapui")
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- setup keymapping
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+	end,
+}
