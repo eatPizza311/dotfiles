b9bea22e5aa36e0ddc23e2fb00e17304a5ec9d73
ChihYing <spacetime0311@gmail.com>
2024-01-17 22:31:06 +0800

    feat: setup Rust LSP & DAP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,20 +9,20 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
+				ensure_installed = { "lua_ls", "rust_analyzer" },
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-            
+
 			lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            
+				capabilities = capabilities,
+			})
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
