f93c0d8c6696b1f22b15db33c32d5050d9750d87
Chih Ying <spacetime0311@gmail.com>
2024-01-18 11:12:57 +0800

    feat: set line number and ruler

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,9 +3,13 @@ vim.cmd("set expandtab")
 vim.cmd("set softtabstop=4")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
-
 -- Changing leader
 vim.g.mapleader = " "
-
 -- Escap using jk
 vim.keymap.set("i", "jk", "<ESC>", {})
+-- Line number
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+-- 80 ruler
+vim.opt.colorcolumn = "80"
