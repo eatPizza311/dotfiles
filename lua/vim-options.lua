8cd2a5861576c4af2577453c08ba7d8daedec5ce
Chih Ying <spacetime0311@gmail.com>
2024-01-30 14:25:08 +0800

    feat: Set line number highlight

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,6 +10,11 @@ vim.keymap.set("i", "jk", "<ESC>", {})
 -- Line number
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "number"
+vim.opt.signcolumn = "auto"
+-- Set cursor line highlighting
+vim.cmd([[
+  hi CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+  set cursorline
+]])
 -- 80 ruler
 vim.opt.colorcolumn = "80"
