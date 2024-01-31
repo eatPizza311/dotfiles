75b9cd8060a2afb135a113376e5d04929831ce39
ChihYing <spacetime0311@gmail.com>
2024-01-31 22:03:29 +0800

    feat: Gitsigns

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,35 +1,37 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
 
-	config = function()
-		local wk = require("which-key")
-		wk.register({
-			f = {
-				name = "file", -- optional group name
-				f = "Find File", -- just a label. don't create any mapping
-				g = "Live Grep",
-				b = "List Buffers",
-				h = "Help Tags",
-			},
-			g = {
-				name = "general",
-				f = "Format",
-				d = "Definition",
-			},
-			d = {
-				name = "debug",
-				t = "Breakpoint",
-				c = "Continue",
-			},
-			c = {
-				name = "code",
-				a = "Action",
-			},
+    config = function()
+        local wk = require("which-key")
+        wk.register({
+            f = {
+                name = "file", -- optional group name
+                f = "Find File", -- just a label. don't create any mapping
+                g = "Live Grep",
+                b = "List Buffers",
+                h = "Help Tags",
+            },
+            g = {
+                name = "git/general",
+                f = "Format",
+                d = "Definition",
+                t = "Toggle line blame",
+                p = "Preview hunk",
+            },
+            d = {
+                name = "debug",
+                t = "Breakpoint",
+                c = "Continue",
+            },
+            c = {
+                name = "code",
+                a = "Action",
+            },
             r = {
                 name = "rust",
                 c = {
@@ -40,8 +42,8 @@ return {
                     R = "Open repository",
                     D = "Open documentation",
                     C = "Open crate.io",
-                }
-            }
-		}, { prefix = "<leader>" })
-	end,
+                },
+            },
+        }, { prefix = "<leader>" })
+    end,
 }
