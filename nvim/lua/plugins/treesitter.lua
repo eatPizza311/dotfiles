beeb7a1667e45bb7551cbb41dd4fcfb53ccefc94
ChihYing <spacetime0311@gmail.com>
2024-04-06 17:11:42 +0800

    feat: incremental selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,6 +8,15 @@ return {
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<Leader>ss",
+                    node_incremental = "<Leader>si",
+                    scope_incremental = "<Leader>sc",
+                    node_decremental = "<Leader>sd",
+                },
+            },
         })
     end,
 }
