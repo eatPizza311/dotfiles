749b93d1734cdcda39189e781e63beb50554a2d6
ChihYing <spacetime0311@gmail.com>
2024-04-11 17:25:36 +0800

    fix: typo

--- nvim/lua/chih/core/keymaps.lua
+++ nvim/lua/chih/core/keymaps.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = " "
 
 -- Vim keymaps
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search hoghlights" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- Window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
