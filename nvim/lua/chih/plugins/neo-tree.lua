65409d0662b1fe6d3fab65a461084595a0e129ed
ChihYing <spacetime0311@gmail.com>
2024-04-11 15:03:22 +0800

    feat: close neovim if neo-tree is the last window

--- nvim/lua/chih/plugins/neo-tree.lua
+++ nvim/lua/chih/plugins/neo-tree.lua
@@ -7,8 +7,13 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+		})
+
 		-- set keymaps
 		local keymap = vim.keymap
-		keymap.set("n", "<C-n>", "<cmd>Neotree filesystem toggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set("n", "<leader>ee", "<cmd>Neotree filesystem toggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set("n", "<leader>ef", "<cmd>Neotree reveal<CR>", { desc = "Toggle file explorer on current file" })
 	end,
 }
