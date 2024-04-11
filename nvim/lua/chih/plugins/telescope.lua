d87251755c521115e6a9a329e1bb3f01b9855d5a
ChihYing <spacetime0311@gmail.com>
2024-04-11 16:55:48 +0800

    feat: add fuzzy finding and more keymaps

--- nvim/lua/chih/plugins/telescope.lua
+++ nvim/lua/chih/plugins/telescope.lua
@@ -1,14 +1,46 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			"nvim-tree/nvim-web-devicons",
+		},
 		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+
+			telescope.setup({
+				defaults = {
+					path_display = { "smart" },
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous, -- move to prev result
+							["<C-j>"] = actions.move_selection_next, -- move to next result
+							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						},
+					},
+				},
+			})
+
+			telescope.load_extension("fzf")
+
+			-- set keymaps
+			local keymap = vim.keymap
+			keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files in cwd" })
+			keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Find recent files" })
+			keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Find help" })
+			keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
+			keymap.set(
+				"n",
+				"<leader>fc",
+				"<cmd>Telescope grep_string<CR>",
+				{ desc = "Find string under cursor in cwd" }
+			)
 		end,
 	},
 	{
