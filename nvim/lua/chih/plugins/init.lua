4b27db38c1ffa3124ee3ba343eaf8b5870d03436
ChihYing <spacetime0311@gmail.com>
2024-04-11 11:58:47 +0800

    chore: move tmux-navigator to plugins/init.lua

---
+++ nvim/lua/chih/plugins/init.lua
@@ -0,0 +1,5 @@
+-- plugins that don't require much configuration
+return {
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+}
