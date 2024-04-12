9e293c7c219f17d127c8581e76a4f1017dbf5c04
ChihYing <spacetime0311@gmail.com>
2024-04-12 16:05:55 +0800

    feat: autopair with cmp

--- nvim/lua/chih/plugins/autopair.lua
+++ nvim/lua/chih/plugins/autopair.lua
@@ -1,5 +1,24 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	opts = {}, -- this is equalent to setup({}) function
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        local autopairs = require("nvim-autopairs")
+
+        autopairs.setup({
+            chech_ts = true, -- enable treesitter
+            ts_config = {
+                lua = { "string" }, -- don't add pairs in lua string treesitter node
+            },
+        })
+
+        -- import nvim-autopairs completion functionality
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        -- import nvim-cmp plugin (completions plugin)
+        local cmp = require("cmp")
+        -- make autopairs and completion work together
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
 }
