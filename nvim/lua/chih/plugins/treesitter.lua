546fc5e2a5542bb69466162d116dc12b9fdd5604
ChihYing <spacetime0311@gmail.com>
2024-04-12 10:54:03 +0800

    feat: auto tag

--- nvim/lua/chih/plugins/treesitter.lua
+++ nvim/lua/chih/plugins/treesitter.lua
@@ -1,14 +1,33 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
         build = ":TSUpdate",
+        dependencies = {
+            "windwp/nvim-ts-autotag",
+        },
         config = function()
             local config = require("nvim-treesitter.configs")
             config.setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rust" },
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "python",
+                    "rust",
+                    "yaml",
+                    "markdown",
+                    "markdown_inline",
+                    "bash",
+                    "dockerfile",
+                },
                 auto_install = true,
                 highlight = { enable = true },
                 indent = { enable = true },
+                autotag = { enable = true },
+
                 incremental_selection = {
                     enable = true,
                     keymaps = {
