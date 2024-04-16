80e4e9bfe3d73c519dcb78421e30322ff6acee25
ChihYing <spacetime0311@gmail.com>
2024-04-16 08:17:18 +0800

    feat: enable rust inlay hint by default

--- nvim/lua/chih/plugins/lsp/rustaceanvim.lua
+++ nvim/lua/chih/plugins/lsp/rustaceanvim.lua
@@ -1,5 +1,14 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^3", -- Recommended
-	ft = { "rust" },
+    "mrcjkb/rustaceanvim",
+    version = "^3", -- Recommended
+    ft = { "rust" },
+    config = function()
+        vim.g.rustaceanvim = {
+            server = {
+                on_attach = function(client, bufnr)
+                    vim.lsp.inlay_hint.enable(bufnr)
+                end,
+            },
+        }
+    end,
 }
