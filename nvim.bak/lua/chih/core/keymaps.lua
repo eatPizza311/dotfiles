22cc5d28a4516f71a2b3b85e8d01e3fd810e86c6
ChihYing <spacetime0311@gmail.com>
2024-11-05 22:52:06 +0800

    feat: start migration to LazyVim

--- nvim.bak/lua/chih/core/keymaps.lua
+++ nvim.bak/lua/chih/core/keymaps.lua
@@ -1,8 +1,10 @@
 -- For conciseness
 local keymap = vim.keymap
 
--- Changes leader key to space
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 -- Vim keymaps
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
